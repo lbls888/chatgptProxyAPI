0449fd6f8dd8e88a63c5007f73d348a769e22f8c
lbls888 <131499083+lbls888@users.noreply.github.com>
2024-07-29 19:09:29 +0800

    Update cf_worker.js

--- cf_worker.js
+++ cf_worker.js
@@ -1,4 +1,4 @@
-const TELEGRAPH_URL = 'https://api.openai.com';
+const TELEGRAPH_URL = 'https://ark.cn-beijing.volces.com/api/v3';
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
